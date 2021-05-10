83ec1fa2259470e796d75820ca106f5ff794a243
Ifeanyi Nwodo <joshuasearch@gmail.com>
2021-05-10 20:33:25 +0100

    Updated all

---
+++ ItemModel-Nugget/ItemModel-Nugget/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemModel_Nugget
+{
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Microservice-Producer/Microservice-Producer/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using ItemModel_Nugget;
+using Microservice_Producer.IOC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice_Producer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IAzureBusService _azureBusService;
+        public OrderController(IAzureBusService azureBusService)
+        {
+            _azureBusService = azureBusService;
+
+        }
+
+        // POST api/<OrderController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Item item)
+        {
+            await _azureBusService.SendMessageAsync(item);
+            return Ok();
+
+
+
+        }
+
+    }
+}
